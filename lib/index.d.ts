b2afeb2da3dc1c44551313f35a0f9ca156192e8a
oriolb <oriol.busquet@gmail.com>
2023-03-16 11:08:01 +0100

    Setup Package

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+export declare class Result {
+    add(a: number, b: number): number;
+    subtract(a: number, b: number): number;
+    multiply(a: number, b: number): number;
+    divide(a: number, b: number): number;
+}
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export class Result {
+  add(a: number, b: number): number {
+    return a + b;
+  }
+
+  subtract(a: number, b: number): number {
+    return a - b;
+  }
+
+  multiply(a: number, b: number): number {
+    return a * b;
+  }
+
+  divide(a: number, b: number): number {
+    return a / b;
+  }
+}
